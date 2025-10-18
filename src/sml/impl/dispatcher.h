4a060622e912d8c3fca5cac51c6eda3af5c28e96
Ibragim Dzhiblavi <dzhiblavi@gmail.com>
2025-10-18 17:11:03 +0300

    simplify dispatcher

--- src/sml/impl/dispatcher.h
+++ src/sml/impl/dispatcher.h
@@ -11,56 +11,6 @@
 
 namespace sml::impl {
 
-template <size_t TrIdx, typename EId, typename SrcSpec, typename StateSpecs, typename TrTuple>
-int accept(const EId& id, TrTuple& transitions) {
-    static constexpr size_t NumTransitions = std::tuple_size_v<TrTuple>;
-
-    if constexpr (TrIdx == NumTransitions) {
-        return -1;
-    } else {
-        using Tr = std::tuple_element_t<TrIdx, TrTuple>;
-
-        using TrSrcTag = typename Tr::Src::Tag;
-        using TrSrcIds = typename Tr::Src::Ids;
-        using TrEventIds = typename Tr::Event::Ids;
-
-        using SrcTag = typename SrcSpec::Tag;
-        using SrcId = typename SrcSpec::Id;
-
-        using Dst = typename Tr::Dst;
-        using DstId = typename Dst::Id;
-
-        static constexpr bool bypass = std::same_as<BypassStateId, DstId>;
-        using DstSpec = std::conditional_t< //
-            std::same_as<KeepStateId, typename Dst::Id>,
-            SrcSpec,
-            traits::StateSpec<typename Dst::Id, typename Dst::Tag>
-        >;
-
-        static constexpr bool transition_matches = [] {
-            constexpr auto tag_matches = std::same_as<TrSrcTag, SrcTag>;
-            constexpr auto id_matches = tl::Empty<TrSrcIds> || tl::Contains<TrSrcIds, SrcId>;
-            constexpr auto event_matches = tl::Empty<TrEventIds> || tl::Contains<TrEventIds, EId>;
-            return tag_matches && id_matches && event_matches;
-        }();
-
-        if constexpr (transition_matches) {
-            Tr& t = std::get<TrIdx>(transitions);
-
-            if (t.template operator()<SrcId, EId>(SrcId{}, id)) {
-                if constexpr (!bypass) {
-                    static_assert(tl::Contains<StateSpecs, DstSpec>);
-                    return tl::Find<DstSpec, StateSpecs>;
-                }
-            }
-
-            return accept<TrIdx + 1, EId, SrcSpec, StateSpecs>(id, transitions);
-        } else {
-            return accept<TrIdx + 1, EId, SrcSpec, StateSpecs>(id, transitions);
-        }
-    }
-}
-
 template <typename EId, tl::IsList Transitions>
 class Dispatcher {
     using StateSpecs = traits::GetStateSpecs<Transitions>;
@@ -69,26 +19,67 @@ class Dispatcher {
     using EvTransitions = traits::FilterTransitionsByEventId<EId, Transitions>;
     using OutboundStateSpecs = traits::GetSrcSpecs<EvTransitions, StateSpecs>;
 
-    // number of outbound states: unique (M, Id)
     static constexpr size_t NumOutboundStates = tl::Size<OutboundStateSpecs>;
-
-    // construct an injection from all states to outbound states
     static constexpr auto StateInjection = tl::injection(StateSpecs{}, OutboundStateSpecs{});
 
  public:
     explicit Dispatcher(TransitionsTuple* ts) : transitions_{ts} {
         supp::constexprFor<0, NumOutboundStates, 1>([this](auto I) {
-            using Spec = tl::At<I, OutboundStateSpecs>;
-            handlers_[I] = &accept<0, EId, Spec, StateSpecs, TransitionsTuple>;
+            handlers_[I] = &accept<tl::At<I, OutboundStateSpecs>>;
         });
     }
 
     int dispatch(int state_idx, const EId& id) {
         state_idx = static_cast<int>(StateInjection[state_idx]);
-        if (state_idx == -1) {  // NOLINT
+        if (state_idx == -1) {
             return -1;
         }
         return handlers_[state_idx](id, *transitions_);
+    }
+
+    template <typename S>
+    struct MatchesSourceState {
+        template <Transition T>
+        static constexpr bool test() {
+            using TrSrcTag = typename T::Src::Tag;
+            using TrSrcIds = typename T::Src::Ids;
+            constexpr auto tag_matches = std::same_as<TrSrcTag, typename S::Tag>;
+            constexpr auto id_matches =
+                tl::Empty<TrSrcIds> || tl::Contains<TrSrcIds, typename S::Id>;
+            return tag_matches && id_matches;
+        }
+    };
+
+    template <typename SrcSpec>
+    static int accept(const EId& id, TransitionsTuple& transitions) {
+        int dst = -1;
+        auto matcher = [&]<Transition T>(tl::Type<T>) {
+            using SrcId = typename SrcSpec::Id;
+            using Dst = typename T::Dst;
+            using DstId = typename Dst::Id;
+            using DstSpec = std::conditional_t<
+                std::same_as<KeepStateId, DstId>,
+                SrcSpec,
+                traits::StateSpec<DstId, typename Dst::Tag>>;
+
+            if (!std::get<T>(transitions).template operator()<SrcId, EId>(SrcId{}, id)) {
+                return false;
+            }
+
+            if constexpr (std::same_as<BypassStateId, DstId>) {
+                return false;
+            } else {
+                static_assert(tl::Contains<StateSpecs, DstSpec>);
+                dst = tl::Find<DstSpec, StateSpecs>;
+                return true;
+            }
+        };
+
+        // transitions that match both the event and the source state
+        using Ts = tl::Filter<MatchesSourceState<SrcSpec>, EvTransitions>;
+
+        tl::forEachShortCircuit(matcher, Ts{});
+        return dst;
     }
 
  private:
